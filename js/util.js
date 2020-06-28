0f85e15c9547d57493958bd1b2cce838e2baef57
ElenaNarmaniya <narmallada@gmail.com>
2020-06-28 21:24:06 +0300

    удаление пробела в модуле util.js

--- js/util.js
+++ js/util.js
@@ -1,10 +1,9 @@
-﻿'use strict';
+'use strict';
 
 (function () {
   var imgUploadForm = document.querySelector('.img-upload__form'); // большая форма загрузки и редактирования фото на 33 строке
   var imgUploadOverlay = imgUploadForm.querySelector('.img-upload__overlay'); // поле для редактирования фото 42 строка
   var bodyModalOpen = document.querySelector('body'); // доступ к  body
-  
 
   window.util = {
     imgUploadForm: imgUploadForm,
