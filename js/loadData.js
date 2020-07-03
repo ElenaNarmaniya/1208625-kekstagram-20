d55d21d867bc3114b8eaf94981b192b439ae6fb2
ElenaNarmaniya <narmallada@gmail.com>
2020-07-03 21:48:32 +0300

    прием данных с сервера

--- js/loadData.js
+++ js/loadData.js
@@ -1,11 +1,12 @@
 'use strict';
 
+// загрузка данных с сервера
 (function () {
   window.loadData = function (callback) {
     var xhr = new XMLHttpRequest();
     xhr.addEventListener('load', function () {
-      var photos = JSON.parse(xhr.responseText);
-      callback(photos);
+      var data = JSON.parse(xhr.responseText);
+      callback(data);
     });
     xhr.open('GET', 'https://javascript.pages.academy/kekstagram/data');
     xhr.send();
